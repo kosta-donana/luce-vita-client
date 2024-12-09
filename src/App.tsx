b3195643a5c477ef1a4e8b9fb431199567973320
hhejo <litsmainjof@gmail.com>
2024-12-09 10:56:45 +0900

    feat: 라우터 초기 설정
    Co-authored-by: koreandroid <dlwotjd116@snu.ac.kr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello, world!</div>,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
