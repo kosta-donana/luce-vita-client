2bdd6941d7fd73b3ab47a93e465bc51fd953e1fc
hhejo <litsmainjof@gmail.com>
2024-12-11 15:53:52 +0900

    feat: Header 배경 추가, 크기, 버튼 변경

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,8 +1,8 @@
 export function Header() {
   return (
-    <header>
-      <div>홈</div>
-      <div>마이</div>
+    <header className="flex justify-between p-2 rounded-xl text-3xl bg-gray-100">
+      <button className="aspect-squre p-3 rounded-xl bg-gray-500 text-white">홈</button>
+      <button className="aspect-squre p-3 rounded-xl bg-gray-500 text-white">마이</button>
     </header>
   );
 }
