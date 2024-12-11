28f7d350d991315f90d53c3a1fa6e720fd2a1d55
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-12-11 16:30:32 +0900

    feat: Login 페이지 제목 및 소개글 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,16 @@
 export function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="px-16 py-20 bg-slate-200">
+      {/* 제목 */}
+      <p className="mt-20 text-primary-400 text-[4rem] font-semibold text-center">Luce Vita!</p>
+      {/* 소개글 */}
+      <p className="mt-20 text-primary-400 text-2xl/loose font-medium text-center">
+        여러분의 삶 속에서 모든 여행을 함께하며
+        <br />
+        계획을 세우고, 일정과 예산을 관리하기 위한
+        <br />
+        서비스를 제공합니다.
+      </p>
+    </div>
+  );
 }
