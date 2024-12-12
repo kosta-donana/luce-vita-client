20963bb3ea7a655e1827cc19983db1921ab0a9a8
hhejo <litsmainjof@gmail.com>
2024-12-12 16:19:35 +0900

    feat: Header, Create 라우터 링크 추가

--- src/components/home/CreateTravelButton.tsx
+++ src/components/home/CreateTravelButton.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export function CreateTravelButton() {
   return (
-    <button className="absolute right-0 bottom-0 p-6 aspect-square w-24 rounded-full text-5xl text-neutral-200 bg-primary-500">
-      +
-    </button>
+    <Link to="/travels/create">
+      <button className="absolute right-0 bottom-0 p-6 aspect-square w-24 rounded-full text-5xl text-neutral-200 bg-primary-500">
+        +
+      </button>
+    </Link>
   );
 }
