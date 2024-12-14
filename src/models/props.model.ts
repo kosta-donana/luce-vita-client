139ae36ad95f380ed707259f41cd6ed3048a93c5
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-12-14 09:11:30 +0900

    feat: iconInfo 타입이 click 이벤트에 대한 콜백 함수를 선택적으로 가질 수 있도록 변경

--- src/models/props.model.ts
+++ src/models/props.model.ts
@@ -7,7 +7,7 @@ type iconInfo = {
   // Font Awesome 아이콘에 대한 이름 (카멜 표기법)
   id: IconDefinition;
   // click 이벤트에 대한 콜백 함수
-  onClickHandler: () => void;
+  onClickHandler?: () => void;
   // 아이콘의 의미에 대한 설명 (Accessibility 관련)
   title: string;
 };
