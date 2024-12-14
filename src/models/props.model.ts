21d4b0290ad6303c8fcf30d6e2c8ae125c758664
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-12-14 17:41:22 +0900

    feat: iconInfo 타입 내용 변경 및 타입 이름을 IconInfo로 변경

--- src/models/props.model.ts
+++ src/models/props.model.ts
@@ -3,17 +3,17 @@
  */
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
-type iconInfo = {
+type IconInfo = {
   // Font Awesome 아이콘에 대한 이름 (카멜 표기법)
   id: IconDefinition;
-  // click 이벤트에 대한 콜백 함수
-  onClickHandler?: () => void;
   // 아이콘의 의미에 대한 설명 (Accessibility 관련)
   title: string;
+  // 라우터 경로
+  route?: string;
 };
 
 export type TopNavProps = {
-  iconInfos: iconInfo[];
+  iconInfos: IconInfo[];
   bgColor: string;
   iconColor: string;
   title: string;
