b53b9741050542c2bc1fea5564fd669d7cc335ad
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-12-17 10:02:36 +0900

    feat: 회원가입 화면 제목 추가

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,8 @@
 export function SignUp() {
-  return <div className="px-9 py-10 bg-primary-400 min-h-full"></div>;
+  return (
+    <div className="px-10 py-16 bg-primary-400 min-h-full">
+      {/* 제목 */}
+      <h1 className="text-slate-50 text-4xl font-medium">이메일 회원가입</h1>
+    </div>
+  );
 }
