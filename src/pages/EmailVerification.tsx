666835151d1512f0450a53db8b9ad3549c4ceeb9
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-12-18 18:13:56 +0900

    feat: EmailVerification 페이지 컴포넌트 생성 및 라우터에 추가

---
+++ src/pages/EmailVerification.tsx
@@ -0,0 +1,8 @@
+export function EmailVerification() {
+  return (
+    <div className="px-10 py-16 bg-primary-400 min-h-full">
+      {/* 제목 */}
+      <h1 className="text-slate-50 text-5xl font-medium">이메일 인증</h1>
+    </div>
+  );
+}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 export { Home } from './Home';
 export { Login } from './Login';
 export { SignUp } from './SignUp';
+export { EmailVerification } from './EmailVerification';
 export { Travels } from './Travels';
 export { TravelDetail } from './TravelDetail';
