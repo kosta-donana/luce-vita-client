40e90050d19af420beeed74a56dcd062ed5cfa9c
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-12-18 20:54:57 +0900

    feat: 다음 단계로 버튼에 대한 handleSubmit() 함수 구현

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,6 @@
-import { useRef, useLayoutEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { requestSignUp } from '../utils/auth.util';
+import { useRef, useLayoutEffect } from 'react';
+import { SignUpStatus, requestSignUp } from '../utils/signup.util';
 import { InputItem } from '../components/signup/InputItem';
 import { HalfWidthButton as Button } from '../components/common/HalfWidthButton';
 
@@ -22,6 +22,7 @@ function validatePassword(password: string): boolean {
 export function SignUp() {
   const navigate = useNavigate();
 
+  const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const passwordConfirmRef = useRef<HTMLInputElement>(null);
   const termsCheckboxRef = useRef<HTMLInputElement>(null);
@@ -30,7 +31,7 @@ export function SignUp() {
     const passwordWarningEl = document.getElementById('password_warning')!;
     const passwordConfirmWarningEl = document.getElementById('password_confirm_warning')!;
 
-    // 비밀번호 입력란 경고 메시지 로직 추가하기
+    // 비밀번호 입력란의 경고 문구 전환 로직 추가하기
     passwordRef.current?.addEventListener('input', () => {
       const [passwordInput, passwordConfirmInput] = [
         passwordRef.current!,
@@ -44,7 +45,7 @@ export function SignUp() {
           : 'hidden';
     });
 
-    // 비밀번호 확인 입력란 경고 메시지 로직 추가하기
+    // 비밀번호 확인 입력란의 경고 문구 전환 로직 추가하기
     passwordConfirmRef.current?.addEventListener('input', () => {
       const [passwordInput, passwordConfirmInput] = [
         passwordRef.current!,
@@ -57,14 +58,16 @@ export function SignUp() {
     });
   }, []);
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+  /**
+   * 다음 단계로 버튼을 클릭했을 때의 동작을 정의하는 함수입니다.
+   */
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const [passwordInput, passwordConfirmInput] = [
       passwordRef.current!,
       passwordConfirmRef.current!,
     ];
-
     // 비밀번호 입력란 유효성 검사하기
     if (!validatePassword(passwordInput.value)) {
       passwordInput.focus();
@@ -72,22 +75,28 @@ export function SignUp() {
       document.getElementById('password_confirm_warning')!.style.visibility = 'hidden';
       return;
     }
-
     // 비밀번호 확인 입력란 유효성 검사하기
     if (passwordInput.value !== passwordConfirmInput.value) {
       passwordConfirmInput.focus();
       return;
     }
-
     // 약관 확인 항목 유효성 검사하기
     if (!termsCheckboxRef.current!.checked) {
+      alert('약관의 내용을 읽고, 체크박스에 동의하지 않으시면 회원가입을 하실 수 없습니다.');
       return;
     }
 
-    if (requestSignUp() === 'success') {
-      navigate('/');
-    } else {
-      // TODO: 에러 메시지 표시하기
+    const status: SignUpStatus = await requestSignUp(emailRef.current!.value, passwordInput.value);
+    switch (status) {
+      case 'verify':
+        navigate('/emailverification');
+        break;
+      case 'exist':
+        alert('이미 등록된 이메일입니다.');
+        break;
+      case 'error':
+        alert('서버 오류가 발생하였습니다.');
+        break;
     }
   }
 
@@ -97,7 +106,14 @@ export function SignUp() {
       <h1 className="text-slate-50 text-5xl font-medium">이메일 회원가입</h1>
       <form className="mt-16" method="post" onSubmit={handleSubmit}>
         {/* 이메일 입력란 */}
-        <InputItem required type="email" margin="mr-24" name="email" title="이메일" />
+        <InputItem
+          ref={emailRef}
+          required
+          type="email"
+          margin="mr-24"
+          name="email"
+          title="이메일"
+        />
         {/* 비밀번호 입력란 */}
         <div className="mt-11 flex">
           <InputItem ref={passwordRef} required type="password" name="password" title="비밀번호" />
@@ -125,7 +141,7 @@ export function SignUp() {
             <br />ⓘ 입력하신 비밀번호가 일치하지 않습니다.
           </p>
         </div>
-        {/* 약관 확인 */}
+        {/* 약관 확인 항목 */}
         <section className="mr-24 mt-11">
           <h1 className="my-2 text-primary-100 text-lg">약관 확인</h1>
           <textarea
@@ -136,7 +152,7 @@ export function SignUp() {
             약관 내용
           </textarea>
           <label className="mt-1 flex items-center">
-            <input ref={termsCheckboxRef} type="checkbox" name="isTermsAgreed" />
+            <input ref={termsCheckboxRef} type="checkbox" name="areTermsAgreed" />
             <p className="ml-1.5 text-primary-100">
               위 약관을 전부 이해하였으며, 상기 모든 내용에 동의합니다.
             </p>
