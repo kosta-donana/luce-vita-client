ccc5bf46959bea80b48b159ce468cf01ca423844
hhejo <litsmainjof@gmail.com>
2024-12-19 17:46:55 +0900

    feat: 여행 상세 페이지 내비게이션 바 추가

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,3 +1,19 @@
+import { TopNav } from '../components/common/TopNav';
+import { faAngleLeft, faUser } from '@fortawesome/free-solid-svg-icons';
+
 export function Todo() {
-  return <div>여행 일정 상세 페이지</div>;
+  return (
+    <div className="p-6 bg-primary-100 min-h-full flex flex-col gap-5">
+      <TopNav
+        navIconInfos={[
+          { id: faAngleLeft, title: '커뮤니티 기능으로 임시 이동하기', route: '/' },
+          { id: faUser, title: '마이페이지로 이동하기', route: '/' },
+        ]}
+        bgColor="bg-secondary-500"
+        iconColor="text-gray-600"
+        title="일정 상세"
+        titleColor="text-white"
+      />
+    </div>
+  );
 }
