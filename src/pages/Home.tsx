3cdb518598f5517bcadd648caf6f4d2d3c92b8f5
hhejo <litsmainjof@gmail.com>
2024-12-20 16:33:51 +0900

    add: dummyTravels 유틸 파일 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,42 +2,12 @@ import { useState } from 'react';
 import { faHouseFlag, faUser, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { withNavigation } from './withNavigation';
 import { Travel } from '../models/travel.model';
+import { dummyTravels } from '../utils/dummy-travels.util';
 import { TopNav } from '../components/common/TopNav';
 import { FloatingNavButton } from '../components/common/FloatingNavButton';
 import { TravelStatus } from '../components/home/TravelStatus';
 import { TravelCard } from '../components/home/TravelCard';
 import { EmptyCard } from '../components/home/EmptyCard';
-
-const dummyTravels: Array<Travel> = [
-  {
-    travelid: 11,
-    userid: '10dhdof03hd9dh',
-    country: { countryname: '대한민국', currency: 'KRW' },
-    title: '제목이다이것이',
-    startdate: '1202',
-    enddate: '1207',
-    localname: '금천구',
-    address: '자세한주소인것임',
-    travelimg: 'https://??????',
-    budgettotal: 110000,
-    tags: ['태그1'],
-    memo: '메모별것도없네이거',
-  },
-  {
-    travelid: 22,
-    userid: '32dkch109fbos6',
-    country: { countryname: '대한민국', currency: 'KRW' },
-    title: '이것도여행인것인거임',
-    startdate: '1209',
-    enddate: '1214',
-    localname: '디지털단지',
-    address: '호서호서호서호서구내식당당',
-    travelimg: 'https://!!!!!!',
-    budgettotal: 220000,
-    tags: ['태그2', '태그3'],
-    memo: '메모를쓰지않은것이냐어째서',
-  },
-];
 
 export const Home = withNavigation(() => {
   const [travels] = useState<Array<Travel>>(dummyTravels);
