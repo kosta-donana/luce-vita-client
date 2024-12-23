28f08a49db14fcd8cf2774441110fe6b1c30b1ef
hhejo <litsmainjof@gmail.com>
2024-12-23 16:21:28 +0900

    add: Axios api 인스턴스 파일 생성

---
+++ src/api/client.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+export const client = axios.create({
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  // withCredentials: true,
+});
+
+// 요청 인터셉터 설정
+client.interceptors.request.use(
+  (config) => {
+    // 요청 보내기 전 수행할 작업
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 설정
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      // 인증 에러 처러
+    }
+    return Promise.reject(error);
+  }
+);
