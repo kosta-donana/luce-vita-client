b2675c94d67d1a1c6fd60ef28a11dab78ae89410
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-12-23 15:34:43 +0900

    fix: 이전 화면으로 되돌아가는 로직 수정

--- src/components/common/GoBack.tsx
+++ src/components/common/GoBack.tsx
@@ -1,12 +1,20 @@
-import { useLayoutEffect } from 'react';
+import { useRef, useLayoutEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const GoBack: React.FC = () => {
   const navigate = useNavigate();
+  const isNavigationExecutedRef = useRef(false);
 
   useLayoutEffect(() => {
-    navigate(-1);
-  });
+    if (!isNavigationExecutedRef.current) {
+      navigate(-1);
+      isNavigationExecutedRef.current = true;
+    }
+
+    return () => {
+      isNavigationExecutedRef.current = false;
+    };
+  }, [navigate]);
 
   return <></>;
 };
