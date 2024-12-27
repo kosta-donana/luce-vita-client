2a448bd2d263aa7d8888694ca261e5c1bd724a60
hhejo <litsmainjof@gmail.com>
2024-12-27 13:22:57 +0900

    feat: 여행 상태 메시지 적용

--- src/components/home/StatusCard.tsx
+++ src/components/home/StatusCard.tsx
@@ -17,7 +17,15 @@ export function StatusCard({ startDate, endDate }: StatusCardProps) {
   return (
     <div className="flex flex-col justify-center my-7 p-6 gap-3 rounded-3xl shadow-md shadow-gray-400 bg-primary-400">
       {/* 일정 상태 */}
-      <div className="my-1.5 w-full text-left text-4xl font-bold text-white">즐거운 여행이에요</div>
+      <div className="my-1.5 w-full text-left text-4xl font-bold text-white">
+        {dday > 0 ? (
+          <span>여행이 다가오고 있어요</span>
+        ) : dday === 0 ? (
+          <span>여행을 떠나요!</span>
+        ) : (
+          <span>즐거운 여행이 계속돼요</span>
+        )}
+      </div>
       {/* 캘린더 */}
       <div className="w-full grid grid-rows-3 grid-cols-7 gap-3 px-2">
         {weeks.flat().map((date, i) => (
